9868f1ef1c2b0c873459c917a74d956f37fc63fa
Akash Tureha <2020kucp1138@iiitkota.ac.in>
2022-02-13 00:13:30 +0530

    Added loader

--- src/App.js
+++ src/App.js
@@ -12,29 +12,39 @@ import Otp from "./registration/otp";
 import { geolocated } from "react-geolocated";
 
 function App(props) {
-  console.log(props?.coords?.latitude);
-
-  return (
-    <>
-      {/* <Navbar /> */}
-      <Routes>
-        <Route path="/hostels" element={<HostelList />} />
-        <Route path="/hostels/:id" element={<HostelDetail />} />
-        <Route path="/" element={<Landing />} />
-        <Route path="/landing" element={<Landing />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/otp" element={<Otp />} />
-        <Route path="/:user/dashboard" element={<Dashboard />} />
-        <Route path="/:user/leave" element={<LeaveApplication />} />
-        <Route path="/:user/issue" element={<RoomIssue />} />
-        <Route path="/:user/feedback" element={<FeedbackForm />} />
-        <Route path="/:user/update-profile" element={<UpdateProfile />} />
-        {/* <Route path="/:user/pay-fee" element={<PayFee />} /> */}
-      </Routes>
-      {/* <Footer /> */}
-    </>
-  );
+  console.log(props.coords);
+  if (!props.coords)
+    return (
+      <div className="w-full h-full flex justify-center align-middle">
+        <img
+          className=""
+          alt="loader"
+          src={require("./assets/img/loading.gif")}
+        />
+      </div>
+    );
+  else
+    return (
+      <>
+        {/* <Navbar /> */}
+        <Routes>
+          <Route path="/hostels" element={<HostelList />} />
+          <Route path="/hostels/:id" element={<HostelDetail />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/landing" element={<Landing />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/otp" element={<Otp />} />
+          <Route path="/:user/dashboard" element={<Dashboard />} />
+          <Route path="/:user/leave" element={<LeaveApplication />} />
+          <Route path="/:user/issue" element={<RoomIssue />} />
+          <Route path="/:user/feedback" element={<FeedbackForm />} />
+          <Route path="/:user/update-profile" element={<UpdateProfile />} />
+          {/* <Route path="/:user/pay-fee" element={<PayFee />} /> */}
+        </Routes>
+        {/* <Footer /> */}
+      </>
+    );
 }
 
 export default geolocated({

040049423d39711cc999132da0046fec05e80b24
Akash Tureha <2020kucp1138@iiitkota.ac.in>
2022-02-13 03:41:56 +0530

    Added Logout

--- src/student/feedbackform.js
+++ src/student/feedbackform.js
@@ -1,48 +1,47 @@
-import React, { useState } from 'react'
-import TextField from '@material-ui/core/TextField';
-import Rating from '@mui/material/Rating';
-import baseurl from "../config"
-import axios from "axios"
-import Typography from '@mui/material/Typography';
-import localStorageKey from "../utils/localStorageKey"
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import Rating from "@mui/material/Rating";
+import baseurl from "../config";
+import axios from "axios";
+import Typography from "@mui/material/Typography";
+import localStorageKey from "../utils/localStorageKey";
 
 function FeedbackForm() {
-    const [value, setValue] = useState(0)
-    const [review, setReview] = useState("")
-    const handleSubmit = async(e) => {
-        e.preventDefault()
-        if(localStorage.getItem('hostelid')) {
-            const res = await axios.post(`${baseurl}/student/Feedback`,
-            {
-                studentid: localStorage.getItem('id'),
-                name: localStorage.getItem('name'),
-                rating: value,
-                message: review,
-                hostelid: localStorage.getItem('hostelid')
-            },
-            {
-                headers: {
-                    Authorization: localStorage.getItem('jwtToken') ? `Bearer ${localStorage.getItem('jwtToken')}` : "",
-                    "Content-type": "application/json",
-                }
-            })
-            if(res.status === 200) {
-                console.log("Submitted successfully")
-            } else {
-                console.error("Something went wrong!")
-            }
+  const [value, setValue] = useState(0);
+  const [review, setReview] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (localStorage.getItem("hostelid")) {
+      const res = await axios.post(
+        `${baseurl}/student/Feedback`,
+        {
+          studentid: localStorage.getItem("id"),
+          name: localStorage.getItem("name"),
+          rating: value,
+          message: review,
+          hostelid: localStorage.getItem("hostelid"),
+        },
+        {
+          headers: {
+            Authorization: localStorage.getItem("jwtToken")
+              ? `Bearer ${localStorage.getItem("jwtToken")}`
+              : "",
+            "Content-type": "application/json",
+          },
         }
-        
-    }
-    const handleReviewChange = (e) => {
-        e.preventDefault()
-        setReview(e.target.value)
+      );
+      if (res.status === 200) {
+        console.log("Submitted successfully");
+      } else {
+        console.error("Something went wrong!");
+      }
     }
   };
   const handleReviewChange = (e) => {
     e.preventDefault();
     setReview(e.target.value);
   };
+
   return (
     <div className="bg-room-issue h-screen bg-cover">
       <h1 className="text-4xl mt-2 text-center">Feedback Form</h1>
@@ -84,6 +83,6 @@ function FeedbackForm() {
       </div>
     </div>
   );
-
+}
 
 export default FeedbackForm;

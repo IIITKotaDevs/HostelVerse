0431fddd0016c1a4a9e7bfe65dd9bab0a70b07b9
VinVash <2020kucp1046@iiitkota.ac.in>
2022-02-13 06:31:55 +0530

    announcement by warden

--- src/warden/putAnnouncement.js
+++ src/warden/putAnnouncement.js
@@ -1,7 +1,91 @@
-import React from 'react'
+import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import baseurl from "../config";
+import axios from "axios";
 
-export default function PutAnnouncement() {
-    return (
-        <div>Hello</div>
-    )
+function PutAnnouncement() {
+  const [heading, setHeading] = useState("");
+  const [content, setContent] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+        const res = await axios.post(
+        `${baseurl}/warden/announcement`,
+        {
+            hostelid: localStorage.getItem('hostelid'),
+            wardenid: localStorage.getItem('wardenid'),
+            heading: heading,
+            message: content
+        },
+        {
+            headers: {
+            Authorization: localStorage.getItem("jwtToken")
+                ? `Bearer ${localStorage.getItem("jwtToken")}`
+                : "",
+            "Content-type": "application/json",
+            },
+        }
+        );
+        
+        setContent("")
+        setHeading("")
+        if (res.status === 200) {
+        console.log("Submitted successfully");
+        } else {
+        console.error("Something went wrong!");
+        }
+    }
+  const handleContentChange = (e) => {
+    e.preventDefault();
+    setContent(e.target.value);
+  };
+  const handleHeadingChange = (e) => {
+    e.preventDefault();
+    setHeading(e.target.value);
+  };
+
+  return (
+    <div className="">
+      <h1 className="text-4xl mt-2 text-center">Make Announcement</h1>
+      <div className="mx-auto text-center w-80 col-span-1">
+        <h1 className="text-center text-2xl mt-20 mb-8">Heading</h1>
+        <TextField
+          id="heading"
+          label="Enter heading..."
+          type="text"
+          multiline
+          onChange={handleHeadingChange}
+          defaultValue=""
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+      </div>
+
+      <div className="mx-auto text-center w-80 col-span-1">
+        <h1 className="text-center text-2xl mt-20 mb-8">Content</h1>
+        <TextField
+          id="content"
+          label="Enter content..."
+          type="text"
+          multiline
+          onChange={handleContentChange}
+          defaultValue=""
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+      </div>
+
+      <div className="mx-auto text-center mt-20">
+        <button
+          className="text-white bg-black px-4 py-2 rounded-3xl"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
 }
+
+export default PutAnnouncement;
